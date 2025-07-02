77d1deaf6594ae9976e086915e1b13a758007f9a
htasdemir <htasdemir@doveschools.org>
2025-07-02 14:07:52 -0500

    Create and.v

---
+++ src/and.v
@@ -0,0 +1,18 @@
+module tt_um_and(input clk, input heartbeat_in, output reg pace_out);
+  reg [31:0] timer;
+
+  always @(posedge clk) begin
+    if (heartbeat_in) begin
+      timer <= 0;
+      pace_out <= 0;
+    end else begin
+      timer <= timer + 1;
+      if (timer > THRESHOLD) begin
+        pace_out <= 1; // Trigger pacing pulse
+        timer <= 0;
+      end else begin
+        pace_out <= 0;
+      end
+    end
+  end
+endmodule
